ade38d9c3c55589f052c3250f90499fe0a0ad961
Thomas Ziegler <thomas.ziegler@tegolo.de>
2023-05-04 19:57:49 +0200

    compute second value

--- FirstTest/ConsoleApplication1/ConsoleApplication1.cpp
+++ FirstTest/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,4 +5,6 @@ int main()
 {
     double value{ 10 };
     std::cout << "Twice the value of " << value << " is " << returnTwiceTheValue(value) << std::endl;
+    double value{ 1234567890987654321 };
+    std::cout << "Twice the value of " << value << " is " << returnTwiceTheValue(value) << std::endl;
 }
