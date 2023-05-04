106cbcb780ec2d9db9101237a34deb495fc2d8bd
Thomas Ziegler <thomas.ziegler@tegolo.de>
2023-05-04 19:48:10 +0200

    initial commit first test

---
+++ FirstTest/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,8 @@
+#include "MathUtils.h"
+#include "IOUtils.h"
+
+int main()
+{
+    double value{ 10 };
+    std::cout << "Twice the value of " << value << " is " << returnTwiceTheValue(value) << std::endl;
+}
---
+++ FirstTest/ConsoleApplication1/MathUtils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+template<typename type>
+type returnTwiceTheValue(const type value)
+{
+	return 2 * value;
+}
