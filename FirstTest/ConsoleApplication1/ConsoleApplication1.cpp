39665decddd2f181d8beb9e71a22ac2e16b3b568
Dr. Thomas Ziegler <thomas.ziegler@tegolo.de>
2023-05-07 15:38:57 +0200

    another computation added

--- FirstTest/ConsoleApplication1/ConsoleApplication1.cpp
+++ FirstTest/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,4 +7,6 @@ int main()
     std::cout << "Twice the value of " << value << " is " << returnTwiceTheValue(value) << std::endl;
     double value{ 1234567890987654321 };
     std::cout << "Twice the value of " << value << " is " << returnTwiceTheValue(value) << std::endl;
+    double value{ 99999999999999999999999999999999999 };
+    std::cout << "Twice the value of " << value << " is " << returnTwiceTheValue(value) << std::endl;
 }
